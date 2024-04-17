a10d1c4e18b9c2175e883dd83714f04977908ca5
Pedro Mello <pgm15@duke.edu>
2024-04-17 18:29:18 -0400

    attempt at controlling steppers with input, needs testing

--- steppers/steppers.v
+++ steppers/steppers.v
@@ -1,6 +1,6 @@
-module steppers(JA1, JA2, JA3, JA4, JA7, CLK50MHZ);
+module steppers(JA1, JA2, JA3, JA4, JA7, CLK50MHZ, rotationDirectionChange);
     output JA1, JA2, JA3, JA4, JA7;
-    input CLK50MHZ;
+    input CLK50MHZ, rotationDirectionChange;
 
     assign {JA1, JA2, JA3, JA4} = ctrl;
     assign JA7 = CLK100HZ;
@@ -38,20 +38,33 @@ module steppers(JA1, JA2, JA3, JA4, JA7, CLK50MHZ);
 //    end
     
     always @(posedge CLK100HZ) begin // other direction
-        case(iterCounter)
-                3'b000 : ctrl = 4'b0100;
-                3'b001 : ctrl = 4'b0110;
-                3'b010 : ctrl = 4'b0010;
-                3'b011 : ctrl = 4'b1010;
-                3'b100 : ctrl = 4'b1000;
-                3'b101 : ctrl = 4'b1001;
-                3'b110 : ctrl = 4'b0001;
-                3'b111 : ctrl = 4'b0101;
+        if (rotationDirectionChange) begin
+            case(iterCounter)
+                    3'b000 : ctrl = 4'b0100;
+                    3'b001 : ctrl = 4'b0110;
+                    3'b010 : ctrl = 4'b0010;
+                    3'b011 : ctrl = 4'b1010;
+                    3'b100 : ctrl = 4'b1000;
+                    3'b101 : ctrl = 4'b1001;
+                    3'b110 : ctrl = 4'b0001;
+                    3'b111 : ctrl = 4'b0101;
             endcase
-        if (iterCounter >= 7) begin
-            iterCounter <= 0;
         end else begin
-            iterCounter <= iterCounter + 1;
+            case(iterCounter)
+                    3'b000 : ctrl = 4'b0100;
+                    3'b001 : ctrl = 4'b0101;
+                    3'b010 : ctrl = 4'b0001;
+                    3'b011 : ctrl = 4'b1001;
+                    3'b100 : ctrl = 4'b1000;
+                    3'b101 : ctrl = 4'b1010;
+                    3'b110 : ctrl = 4'b0010;
+                    3'b111 : ctrl = 4'b0110;
+            endcase
         end
+        if (iterCounter >= 7) begin
+                iterCounter <= 0;
+            end else begin
+                iterCounter <= iterCounter + 1;
+            end
     end
 endmodule
